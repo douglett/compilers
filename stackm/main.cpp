0eca6712cabf42975f392fc12957eaa8a8c4b129
doug <doug@d-mini.local>
2018-01-02 19:16:53 +0000

    stack update

--- stackm/main.cpp
+++ stackm/main.cpp
@@ -10,7 +10,8 @@ using namespace std;
 typedef int32_t i32;
 typedef map<string, vector<i32>> LFrame;
 static vector<LFrame> frames;
-static i32 lPC = 0;
+// static vector<LComm>  commands;
+// static i32 lPC = 0;
 
 
 void lframe() {
@@ -28,21 +29,21 @@ void lunframe() {
 // }
 void ldef(const string& id, i32 size) {
 	if (!frames.size())
-		throw "attempt to def on empty stack";
+		throw (string) "attempt to def on empty stack";
 	if (frames.back().count(id))
-		throw "attempt to def existing variable name: " + id;
+		throw (string) "attempt to def existing variable name: " + id;
 	if (size < 1 || size > 256)
-		throw "def size out of range: " + to_string(size);
+		throw (string) "def size out of range: " + to_string(size);
 	frames.back()[id] = vector<i32>(size, 0);
 }
 i32 lgeta(const string& id, i32 pos) {
 	if (!frames.size())
-		throw "attempt to access on empty stack";
+		throw (string) "attempt to access on empty stack";
 	if (frames.back().count(id) == 0)
-		throw "attempt to access value on undefined var: " + id;
+		throw (string) "attempt to access value on undefined var: " + id;
 	auto& v = frames.back()[id];
 	if (pos < 0 || pos >= v.size())
-		throw "index out of range on var " + id + " : " + to_string(pos);
+		throw (string) "index out of range on var " + id + " : " + to_string(pos);
 	return v[pos];
 }
 void lleta(const string& id, i32 pos, i32 val) {
@@ -56,9 +57,6 @@ i32 lget(const string& id) {
 void llet(const string& id, i32 val) {
 	lleta(id, 0, val);
 }
-void llabel(const string& id) {
-	throw "how do I set labels?";
-}
 i32 loper(const string& op, i32 v1, i32 v2) {
 	if      (op == "+" )  return v1 +  v2;
 	else if (op == "-" )  return v1 -  v2;
@@ -71,17 +69,14 @@ i32 loper(const string& op, i32 v1, i32 v2) {
 int main() {
 	try {
 		lframe();
+		ldef("PC", 1);
 		ldef("foo", 1);
 		llet("foo", 20);
 		lleta("foo", 0, 21);
-		// llabel("foolabel");
 		printf("> [%d]\n", loper("+", 10, lget("foo")) );
 		lunframe();
 	}
 	catch (const string& e) {
-		throw e.c_str();
-	}
-	catch (const char* e) {
-		fprintf(stderr, "error: %s\n", e);
+		fprintf(stderr, "error: %s\n", e.c_str());
 	}
 }
