814dab7808f5fc8850df38c89c9319bf4a7ac755
doug <doug@d-mini.local>
2018-01-02 22:23:00 +0000

    env stack

--- stackm/envstack.cpp
+++ stackm/envstack.cpp
@@ -1,32 +1,43 @@
+#include "envstack.h"
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
-#include <cstdint>
 
 using namespace std;
 
 
-typedef int32_t i32;
+//-- private vars
 typedef map<string, vector<i32>> LFrame;
 static vector<LFrame> frames;
 // static vector<LComm>  commands;
 // static i32 lPC = 0;
 
 
+//-- private helpers
+// find var in stack
+static vector<i32>& lgetvar(const string& id) {
+	if (!frames.size())
+		throw (string) "attempt to access on empty stack";
+	for (int i = frames.size()-1; i >= 0; i--)
+		if (frames[i].count(id))
+			return frames[i][id];
+	throw (string) "attempt to access value on undefined var: " + id;
+}
+
+
+//-- public methods
+// push frame
 void lframe() {
 	frames.push_back({});
 }
+// pop frame
 void lunframe() {
 	if (!frames.size()) 
 		throw "attempt to pop frame stack while list is empty";
 	frames.pop_back();
 }
-// LFrame& lgetframe() {
-// 	if (!frames.size())
-// 		throw "attempt to get frame from empty stack";
-// 	return frames.back();
-// }
+// define var in frame
 void ldef(const string& id, i32 size) {
 	if (!frames.size())
 		throw (string) "attempt to def on empty stack";
@@ -36,56 +47,32 @@ void ldef(const string& id, i32 size) {
 		throw (string) "def size out of range: " + to_string(size);
 	frames.back()[id] = vector<i32>(size, 0);
 }
-// helper - find var in stack
-static vector<i32>& lgetvar(const string& id) {
-	if (!frames.size())
-		throw (string) "attempt to access on empty stack";
-	for (int i = frames.size()-1; i >= 0; i--)
-		if (frames[i].count(id))
-			return frames[i][id];
-	throw (string) "attempt to access value on undefined var: " + id;
-}
+// get var at array index
 i32 lgeta(const string& id, i32 pos) {
 	auto& v = lgetvar(id);  // get from stack. does error checking
 	if (pos < 0 || pos >= v.size())
 		throw (string) "index out of range on var " + id + " : " + to_string(pos);
 	return v[pos];
 }
+// set var at array index
 void lleta(const string& id, i32 pos, i32 val) {
 	auto& v = lgetvar(id);  // get from stack. does error checking
 	if (pos < 0 || pos >= v.size())
 		throw (string) "index out of range on var " + id + " : " + to_string(pos);
 	v[pos] = val;
 }
+// get and set at index 0 (simple var)
 i32 lget(const string& id) {
 	return lgeta(id, 0);
 }
 void llet(const string& id, i32 val) {
 	lleta(id, 0, val);
 }
+// run operation
 i32 loper(const string& op, i32 v1, i32 v2) {
 	if      (op == "+" )  return v1 +  v2;
 	else if (op == "-" )  return v1 -  v2;
 	else if (op == "=" || op == "==")  return v1 == v2;
 	else if (op == "!=")  return v1 != v2;
 	else    throw "unknown operator: " + op;
-}
-
-
-int main() {
-	try {
-		lframe();
-			ldef("PC", 1);
-			lframe();
-				ldef("foo", 1);
-				llet("foo", 20);
-				lleta("foo", 0, 21);
-				printf("> [%d]\n", loper("+", 10, lget("foo")) );
-				printf("> [%d]\n", lget("PC") );
-			lunframe();
-		lunframe();
-	}
-	catch (const string& e) {
-		fprintf(stderr, "error: %s\n", e.c_str());
-	}
 }
---
+++ stackm/envstack.h
@@ -0,0 +1,13 @@
+#include <string>
+#include <cstdint>
+
+typedef int32_t i32;
+
+void lframe   ( );
+void lunframe ( );
+void ldef     (const std::string& id, i32 size);
+i32  lgeta    (const std::string& id, i32 pos);
+void lleta    (const std::string& id, i32 pos, i32 val);
+i32  lget     (const std::string& id);
+void llet     (const std::string& id, i32 val);
+i32  loper    (const std::string& op, i32 v1, i32 v2);
